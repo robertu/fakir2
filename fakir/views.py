7da91ede3adf241636d66f842bb9b90a7d7643c0
dawideeq <19dg.dawid.pabis@ezse.pl>
2021-03-10 23:07:36 +0100

    strona poczatkowa z logowaniem

--- fakir/views.py
+++ fakir/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			return redirect('homepage')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'fakir/index.html', {'form': form})
