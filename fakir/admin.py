2406b1bc5d699013bfffe19ee4e11d525f77a516
Bezi2018 <74766508+Bezi2018@users.noreply.github.com>
2021-05-10 13:11:21 +0200

    Licznik,Pozycje ver.1

--- fakir/admin.py
+++ fakir/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Sprzedawca, Kupujacy, Faktura
+from .models import Sprzedawca, Kupujacy, Faktura, LicznikFaktur, PozycjaFaktur
 
 
-
+class PozycjeInline(admin.TabularInline):
+    model = PozycjaFaktur
+    extra = 3
 
 class FakturaAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'kupujący', 'sprzedawca', 'cena', 'data_wystawienia')
+    list_display = ('nazwa', 'kupujący', 'sprzedawca', 'data_wystawienia')
     list_filter = ['data_wystawienia']
-
+    inlines = [PozycjeInline]
 
 class KupujacyAdmin(admin.ModelAdmin):
     list_display = ('pk', 'imie', 'nazwisko', 'PESEL')
@@ -19,3 +21,5 @@ class SprzedawcaAdmin(admin.ModelAdmin):
 admin.site.register(Faktura, FakturaAdmin)
 admin.site.register(Kupujacy, KupujacyAdmin)
 admin.site.register(Sprzedawca, SprzedawcaAdmin)
+admin.site.register(LicznikFaktur)
+admin.site.register(PozycjaFaktur)
--- fakir/models.py
+++ fakir/models.py
@@ -30,27 +30,40 @@ class Kupujacy(models.Model):
         verbose_name = "Kupujący"
         verbose_name_plural = "Kupujący"
 
-class Faktura(models.Model):
-    Nazwa_towaru_uslugi = models.CharField(max_length=200)
-
-    J_M_CHOICES = [
-        ("1", "Godzina"),
-        ("2", "Sztuka"),
-    ]
-
-    J_M = models.CharField("J.N", max_length=200, choices=J_M_CHOICES, default="2")
-    ilosc = models.IntegerField()
-    cena = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    wartosc_netto = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+class LicznikFaktur(models.Model):
+    nazwa = models.CharField(max_length=200, null=True)
     podatek = models.IntegerField()
-    wartosc_brutto = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nazwa
+
+class Faktura(models.Model):
+
+    nazwa = models.CharField(max_length=200, null=True)
     kupujący = models.ForeignKey(Kupujacy, on_delete=models.CASCADE)
     sprzedawca = models.ForeignKey(Sprzedawca, on_delete=models.CASCADE)
+    licznik = models.ForeignKey(LicznikFaktur, on_delete=models.CASCADE)
     data_wystawienia = models.DateTimeField('Data wystawienia')
 
-    # def __str__(self):
-    #     return self.pk
+
+    def __str__(self):
+        return self.nazwa
 
     class Meta:
         verbose_name = "Faktura"
         verbose_name_plural = "Faktury"
+
+
+
+class PozycjaFaktur(models.Model):
+    faktura = models.ForeignKey(Faktura, on_delete=models.CASCADE)
+    Nazwa_towaru_uslugi = models.CharField(max_length=200)
+    J_M_CHOICES = [
+        ("1", "Godzina"),
+        ("2", "Sztuka"),
+    ]
+    J_M = models.CharField("J.M", max_length=200, choices=J_M_CHOICES, default="2")
+    ilosc = models.IntegerField()
+    cena = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    wartosc_netto = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    wartosc_brutto = models.DecimalField(max_digits=20, decimal_places=2, default=0)
