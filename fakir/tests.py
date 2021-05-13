43a130b611ae6562a38eae449056101f5e5ac6be
patryk <79578683+PatrykSowinski@users.noreply.github.com>
2021-05-13 13:05:54 +0200

    Added test

--- fakir/tests.py
+++ fakir/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
+import django
+django.setup()
+from .models import NumeracjaFaktur, LicznikFaktur
 
 # Create your tests here.
+def test_licznika():
+    nr = NumeracjaFaktur.objects.create(nazwa='test', wzorzec='{d}/{m}/{r}/{n}')
+    assert nr.numer() == '12/5/2021/0'
