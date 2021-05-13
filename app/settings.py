bdeb7c7dc632eca9eabb64ca448e8624507b6c54
Robert Urbańczyk <robertu@robertu.net>
2021-05-13 13:52:49 +0200

    defaults

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
